15c8a141b8baae033ba9d39fbe3e14f670d7e897
trisongz <tri@songzcorp.com>
2021-09-29 15:12:04 -0500

    Debugging Logic

--- components/crud-web-apps/jupyter/backend/apps/common/routes/get.py
+++ components/crud-web-apps/jupyter/backend/apps/common/routes/get.py
@@ -59,10 +59,6 @@ def get_gpu_vendors():
         v.get("limitsKey", "") for v in gpus_value.get("vendors", [])
     ]
 
-    # Add logic to handle autoscalers, returning all vendors
-    if frontend_config.get("gpus", {}).get('autoscaling', False) is True:
-        return api.success_response("vendors", config_vendor_keys)
-
     # Get all of the different resources installed in all nodes
     installed_resources = set()
     nodes = api.list_nodes().items
@@ -71,5 +67,12 @@ def get_gpu_vendors():
 
     # Keep the vendors the key of which exists in at least one node
     available_vendors = installed_resources.intersection(config_vendor_keys)
+    log.info("Autoscaling Enabled: {}".format(frontend_config.get("gpus", {}).get('autoscaling', False)))
+    # Add logic to handle autoscalers, returning all vendors
+    if frontend_config.get("gpus", {}).get('autoscaling', False) is True:
+        log.info(f"Returning Vendor Keys: {config_vendor_keys}")
+        log.info(f"Installed Resources: {installed_resources}")
+        log.info(f"Node Resources: {nodes}")
+        return api.success_response("vendors", config_vendor_keys)
 
     return api.success_response("vendors", list(available_vendors))
